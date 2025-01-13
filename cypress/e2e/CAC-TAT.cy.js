05abf4441a70e5e6ca31cfd95706a38a60fefd3d
Joao <joaopedro.ovx@gmail.com>
2025-01-13 19:30:24 -0300

    Alteração: Correção do caminho do index.html

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -2,7 +2,7 @@
 
 describe("Central de Atendimento ao Cliente TAT", () => {
   beforeEach(() => {
-    cy.visit("../cypress-do-zero-a-nuvem/src/index.html");
+    cy.visit("../src/index.html");
   });
   it("verifica o título da aplicação", () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
 it('testa a página da política de privacidade de forma independente', () => {
-    cy.visit('./cypress-do-zero-a-nuvem/src/privacy.html')
+    cy.visit('./src/privacy.html')
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
   });
